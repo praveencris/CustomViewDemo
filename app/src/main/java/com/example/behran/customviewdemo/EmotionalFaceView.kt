3caa0cff148437c380bd14f70c46123c7fc96448
Praveen <pkchmits@gmail.com>
2022-02-10 22:27:35 +0530

    Done adding happy and sad faces

--- app/src/main/java/com/example/behran/customviewdemo/EmotionalFaceView.kt
+++ app/src/main/java/com/example/behran/customviewdemo/EmotionalFaceView.kt
@@ -7,22 +7,55 @@ import android.view.View
 
 class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-    // Paint object for coloring and styling
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-
     // Some colors for the face background, eyes and mouth.
-    private var faceColor = Color.YELLOW
-    private var eyesColor = Color.BLACK
-    private var mouthColor = Color.BLACK
-    private var borderColor = Color.BLACK
-
+    private var faceColor = DEFAULT_FACE_COLOR
+    private var eyesColor = DEFAULT_EYES_COLOR
+    private var mouthColor = DEFAULT_MOUTH_COLOR
+    private var borderColor = DEFAULT_BORDER_COLOR
     // Face border width in pixels
-    private var borderWidth = 4.0f
+    private var borderWidth = DEFAULT_BORDER_WIDTH
 
-    // View size in pixels
-    private var size = 320
-
+    // Paint object for coloring and styling
+    private val paint = Paint()
     private val mouthPath = Path()
+    // View size in pixels
+    private var size = 0
+
+    // 3
+    var happinessState = HAPPY
+        set(state) {
+            field = state
+            // 4
+            invalidate()
+        }
+
+    // 5
+    init {
+        paint.isAntiAlias = true
+        setupAttributes(attrs)
+    }
+
+    private fun setupAttributes(attrs: AttributeSet?) {
+        // 6
+        // Obtain a typed array of attributes
+        val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.EmotionalFaceView,
+            0, 0)
+
+        // 7
+        // Extract custom attributes into member variables
+        happinessState = typedArray.getInt(R.styleable.EmotionalFaceView_state, HAPPY.toInt()).toLong()
+        faceColor = typedArray.getColor(R.styleable.EmotionalFaceView_faceColor, DEFAULT_FACE_COLOR)
+        eyesColor = typedArray.getColor(R.styleable.EmotionalFaceView_eyesColor, DEFAULT_EYES_COLOR)
+        mouthColor = typedArray.getColor(R.styleable.EmotionalFaceView_mouthColor, DEFAULT_MOUTH_COLOR)
+        borderColor = typedArray.getColor(R.styleable.EmotionalFaceView_borderColor,
+            DEFAULT_BORDER_COLOR)
+        borderWidth = typedArray.getDimension(R.styleable.EmotionalFaceView_borderWidth,
+            DEFAULT_BORDER_WIDTH)
+
+        // 8
+        // TypedArray objects are shared and must be recycled.
+        typedArray.recycle()
+    }
 
     override fun onDraw(canvas: Canvas?) {
         // call the super method to keep any drawing from the parent side.
@@ -69,13 +102,20 @@ class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, a
     }
 
     private fun drawMouth(canvas: Canvas?) {
-        // 1
-        mouthPath.moveTo(size * 0.22f, size * 0.7f)
-        // 2
-        mouthPath.quadTo(size * 0.50f, size * 0.80f, size * 0.78f, size * 0.70f)
-        // 3
-        mouthPath.quadTo(size * 0.50f, size * 0.90f, size * 0.22f, size * 0.70f)
+        // Clear
+        mouthPath.reset()
 
+        mouthPath.moveTo(size * 0.22f, size * 0.7f)
+
+        if (happinessState == HAPPY) {
+            // Happy mouth path
+            mouthPath.quadTo(size * 0.5f, size * 0.80f, size * 0.78f, size * 0.7f)
+            mouthPath.quadTo(size * 0.5f, size * 0.90f, size * 0.22f, size * 0.7f)
+        } else {
+            // Sad mouth path
+            mouthPath.quadTo(size * 0.5f, size * 0.50f, size * 0.78f, size * 0.7f)
+            mouthPath.quadTo(size * 0.5f, size * 0.60f, size * 0.22f, size * 0.7f)
+        }
         // 4
         paint.color = mouthColor
         paint.style = Paint.Style.FILL
@@ -90,6 +130,20 @@ class EmotionalFaceView(context: Context, attrs: AttributeSet) : View(context, a
         size = Math.min(measuredWidth, measuredHeight)
         // 2
         setMeasuredDimension(size, size)
+    }
+
+
+
+    // 1
+    companion object {
+        private const val DEFAULT_FACE_COLOR = Color.YELLOW
+        private const val DEFAULT_EYES_COLOR = Color.BLACK
+        private const val DEFAULT_MOUTH_COLOR = Color.BLACK
+        private const val DEFAULT_BORDER_COLOR = Color.BLACK
+        private const val DEFAULT_BORDER_WIDTH = 4.0f
+
+        const val HAPPY = 0L
+        const val SAD = 1L
     }
 
 }
