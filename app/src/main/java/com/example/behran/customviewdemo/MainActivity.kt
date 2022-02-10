44c419df67831aadf066919033ed934b0443f8f7
Praveen <pkchmits@gmail.com>
2022-02-11 05:23:31 +0530

    Successfully added user intraction

--- app/src/main/java/com/example/behran/customviewdemo/MainActivity.kt
+++ app/src/main/java/com/example/behran/customviewdemo/MainActivity.kt
@@ -7,5 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val happyButton = findViewById<EmotionalFaceView>(R.id.happyButton)
+        val sadButton = findViewById<EmotionalFaceView>(R.id.sadButton)
+        val emotionalFaceView = findViewById<EmotionalFaceView>(R.id.emotionalFaceView)
+        happyButton.setOnClickListener{
+           emotionalFaceView.happinessState = EmotionalFaceView.HAPPY
+        }
+        sadButton.setOnClickListener{
+            emotionalFaceView.happinessState = EmotionalFaceView.SAD
+        }
     }
 }
